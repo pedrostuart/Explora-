a0fc79821e29ec7b106f568b168badb87bab1a65
pedrostuart <pedrohsmoura897@gmail.com>
2026-01-04 15:21:02 -0300

    ..

--- javascript/preferencias.js
+++ javascript/preferencias.js
@@ -16,3 +16,29 @@ function aplicar(event){
     let dialog = document.getElementById("modal")
     dialog.style.display = 'none'
 }
+
+
+let finalizarCadastro = document.querySelector(".btn-finalizar")
+
+finalizarCadastro.addEventListener("click", CalcularIdade)
+    
+    
+function CalcularIdade(event){
+    event.preventDefault()
+
+    let anoAtual = new Date().getFullYear()
+    let anoIdade = parseInt(document.getElementById("ano").value)
+    let mesIdade = parseInt(document.getElementById("mes").value)
+    let diaIdade = parseInt(document.getElementById('dia').value)
+    let label = document.querySelector("label")
+
+    let idadeDoUsuario = anoAtual - anoIdade
+
+    if(anoIdade > anoAtual || idadeDoUsuario < 6 || mesIdade > 12 || diaIdade > 31 || idadeDoUsuario > 112){
+        label.style.color = 'red'
+        label.innerHTML = 'Data inválida'
+    }else{
+        label.innerText = 'Tudo Ok!'
+        label.style.color = 'green'
+    }
+}
