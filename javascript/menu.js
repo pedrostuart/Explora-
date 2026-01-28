297e9995d0eada0e0d5d9aff99e9236333424ad5
pedrostuart <pedrohsmoura897@gmail.com>
2026-01-27 21:49:58 -0300

    ..

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,12 +1,18 @@
 let hamburguer = document.querySelector(".hamburguer")
+let menu = document.querySelector(".barra-lateral-header")
 
 hamburguer.addEventListener("click", () =>{
-    let menu = document.querySelector(".barra-lateral-header")
     menu.style.display = 'flex'
 })
-let fechar = document.querySelector(".fechar-menu")
 
-fechar.addEventListener("click", () =>{
-    let menu = document.querySelector(".barra-lateral-header")
+let fechar = document.querySelector(".fechar-menu")
+    fechar.addEventListener("click", () =>{
     menu.style.display = 'none'
 })
+
+window.addEventListener("resize", ()=>{ 
+    if (window.innerWidth > 685) {
+    
+        menu.style.display = "none";
+    }
+});
